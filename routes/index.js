bb1c6b44bc6a33b7783a3ae2f0e7206513d4d2f5
lvzi316 <chaopan@outlook.com>
2015-01-26 22:32:17 +0800

    Display json in user readable format

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
+var title;
+var jsondata;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  fs.readFile('package.json', function(err, data) {
+      if (err) 
+      {
+        title = 'Oops';
+      };
+      title = 'resource';
+      res.render('index', { title: 'resource' , resource: JSON.stringify(JSON.parse(data), null, 4)});
+    }
+  )
 });
 
 module.exports = router;
