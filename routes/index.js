c2b93fd4d1f2f622169fd34485ea9e89ceb6a938
lvzi316 <chaopan@outlook.com>
2015-02-05 22:38:02 +0800

    show tables with bootstrap style

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,37 @@ var router = express.Router();
 var fs = require('fs');
 var title;
 var jsondata;
+var syncgroups = [
+  {
+    'name':'mysyncgroup',
+    'status':'created',
+    'createby':'2015-01-29',
+    'detailurl':'/syncgroup/dataset'
+  },
+  {
+    'name':'mysyncgroup2',
+    'status':'created',
+    'createby':'2015-01-30',
+    'detailurl':'/syncgroup/dataset'
+  }
+];
 /* GET home page. */
-router.get('/resource/:name', function(req, res, next) {
+router.get('/', function(req, res, next){
+    res.render('index', {title: 'Sync Groups', syncgroups: syncgroups});
+  }
+);
+
+router.get('/syncgroup/:name', function(req, res, next) {
   var resourcename = req.params.name;
   title = resourcename;
-  console.log('page title: ', title);
   var filename = 'static/' + resourcename + '.json';
   console.log('filename: ', filename);
   var data = fs.readFile(filename, function(err, data){
       var payload = JSON.parse(data);
       console.log(payload);
-      res.render('index', { title: title , resource: JSON.stringify(payload, null, 2)});
+      res.render('detail', { title: title , resource: JSON.stringify(payload, null, 2)});
     }
   );
-
 });
 
 module.exports = router;
