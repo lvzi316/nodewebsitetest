bd96e46ce0e1143b1cd79191a8acdc9b072272e7
lvzi316 <chaopan@outlook.com>
2015-02-06 16:27:30 +0800

    add login page and route. use bootstrap logincss

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 var app = express();
 
 // view engine setup
@@ -21,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', index);
+app.get('/login', login.form)
 
 
 // catch 404 and forward to error handler
